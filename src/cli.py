134b5630cc8c970731de8c8a535acfc5353a6d97
Zacharias <zacharias.thompson@salesforce.com>
2021-08-10 09:42:23 -0700

    Back to user since the docs say that

--- src/cli.py
+++ src/cli.py
@@ -28,7 +28,7 @@ class Action(Enum):
 def build_headers(token: str, user: str, league: League, action: Action):
     return {
         "token": token,
-        "username": user,
+        "user": user,
         "league": league.value,
         "action": action.value,
     }
